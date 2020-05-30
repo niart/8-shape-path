7c650e6a05a184e75c2dd1cceaa1e3d1d1c54c4c
N <49170019+niart@users.noreply.github.com>
2020-05-30 20:18:45 +0200

    Add files via upload

---
+++ time.py
@@ -0,0 +1,48 @@
+import time
+import math
+import itertools
+from scipy import integrate
+import turtle
+
+pi = math.pi
+R = 20
+V = 3
+g = 9.81
+P = 4*pi*R/V #period of each "8"
+#Thrust
+
+def f(x):
+    return V * math.sin(Yaw_velocity * x)
+def f2(x):
+    return V * math.cos(Yaw_velocity * x)
+
+
+win = turtle.Screen()
+turtle.pendown()
+
+Takeoff =time.process_time()
+#for n in itertools.count():
+ #   t = time.process_time()- Takeoff
+while True:
+    t = time.process_time()- Takeoff
+    if int(t/(0.5*P))%2 ==0:
+       #     2 * n * (0.5 * P) <= t < (2 * n + 1) * (0.5 * P):
+        Yaw_velocity = V/R #angular velocity
+        Roll_angle= math.atan(V**2/(g*R)) #
+
+    #elif (2 * n + 1) * (0.5 * P) <= t < (2 * n + 2) * (0.5 * P):
+    else :
+        Yaw_velocity = -V/R #angular velocity
+        Roll_angle = -math.atan(V**2/(g*R))
+
+    print(t, Yaw_velocity, Roll_angle)
+
+    X = integrate.quad(f, 0, t)
+    Y = integrate.quad(f2, 0, t)
+    turtle.goto(X, Y)
+
+
+
+
+
+
